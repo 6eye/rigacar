d0a926d105c7d03bc6bbe92f62dcd9e951cd65ac
David Gayerie <dagaydevel@free.fr>
2018-06-06 23:33:05 +0200

    blender 2.8 port

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ bl_info = {
     "name": "Rigacar (Generates Car Rig)",
     "author": "David Gayerie",
     "version": (3, 1),
-    "blender": (2, 76, 0),
+    "blender": (2, 80, 0),
     "location": "View3D > Add > Armature",
     "description": "Adds a deformation rig for vehicules, generates animation rig and bake wheels animation.",
     "wiki_url": "http://digicreatures.net/articles/rigacar.html",
